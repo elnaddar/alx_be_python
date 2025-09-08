fec0dcc1fd1b4c239c99015b7654847cffdc7b70
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-09-08 18:06:49 +0300

    init commit

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print(f"Addition operation of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction operation of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication operation of {number1} and {number2} is {number1 * number2}")
