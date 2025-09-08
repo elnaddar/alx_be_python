192ff630e3ea5fcad6e5cc857569a31e700870f2
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-09-08 18:34:11 +0300

    fix: variables names

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-m_income = int(input("Enter your monthly income: "))
-m_expenses = int(input("Enter your total monthly expenses: "))
+monthely_income = int(input("Enter your monthly income: "))
+monthely_expenses = int(input("Enter your total monthly expenses: "))
 
-m_savings = m_income - m_expenses
-print(f"Your monthly savings are ${m_savings}.")
+monthely_savings = monthely_income - monthely_expenses
+print(f"Your monthly savings are ${monthely_savings}.")
 
-p_savings = m_savings * 12 + m_savings * 12 * 0.05
+p_savings = monthely_savings * 12 + monthely_savings * 12 * 0.05
 print(f"Projected savings after one year, with interest, is: ${p_savings}.")
