91a21822621703f5ade9375717858ee6fc34f033
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-09-20 11:05:49 +0300

    control flow proj

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,18 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? ")
+start = "Note: "
+end = ""
+
+match priority:
+    case "high":
+        start = "Remainder: "
+    case "medium":
+        start = "Attention: "
+
+if time_bound == "no":
+    end = ". Consider completing it when you have free time."
+else:
+    end = " that requires immediate attention today!"
+
+print(f"{start}'{task}' is a {priority} priority task{end}")
