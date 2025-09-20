1341d656b0b23e9bfa6b07f152da90bfe9c1322a
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-09-20 10:54:14 +0300

    while loop

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,9 @@
 i = size = int(input("Enter the size of the pattern: "))
 
 while i > 0:
-    print("*"*size)
+    j = size
+    while j > 0:
+        print("*", end="")
+        j -= 1
+    print()
     i -= 1
