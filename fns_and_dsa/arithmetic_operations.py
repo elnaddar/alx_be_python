27e2b7bfb73ca82816f2391ec00429ec8f7f2ab4
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-09-28 08:59:52 +0300

    function 1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1:float, num2:float, operation:str):
+    result = 0
+    # add, subtract, multiply, divide
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("Cannot divide by zero.")
+                raise SystemExit
+            result = num1 / num2
+
+    return result
